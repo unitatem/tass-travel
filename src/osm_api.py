61e91dd854e10eef8919e900a80bfec25f6c4ef7
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-12-27 12:20:57 +0100

    Remove code duplicates in OSM API

--- src/osm_api.py
+++ src/osm_api.py
@@ -7,44 +7,68 @@ class OSM:
     def __init__(self):
         self._api = overpy.Overpass()
 
-    def get_poi_cnt_for_city(self, name: str, lat_lng: {float, float}, radius: int):
-        query = """
+        self._query_header = """
         [out:json][timeout:60];
-        node
-          ["name"="{name}"]
-          ["place"]
-          ({s},{w},{n},{e});
+        node["name"="{name}"]["place"]({s},{w},{n},{e});
         (
-          node(around:{radius})["historic"] -> .historic_n;
-          way(around:{radius})["historic"] -> .historic_w;
-          relation(around:{radius})["historic"] -> .historic_r;
-          
-          node(around:{radius})["leisure"] -> .leisure_n;
-          way(around:{radius})["leisure"] -> .leisure_w;
-          relation(around:{radius})["leisure"] -> .leisure_r;
-          
-          node(around:{radius})["tourism"] -> .tourism_n;
-          way(around:{radius})["tourism"] -> .tourism_w;
-          relation(around:{radius})["tourism"] -> .tourism_r;
-          
-          node(around:{radius})["building"="hotel"] -> .hotel_n;
-          way(around:{radius})["building"="hotel"] -> .hotel_w;
-          relation(around:{radius})["building"="hotel"] -> .hotel_r;
-          
-          node(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_n;
-          way(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_w;
-          relation(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_r;
+        """
+        self._query_header_vast = """
+        [out:json][timeout:60];
+        (
+            node["name"="{name}"]["place"]({s},{w},{n},{e}) -> .city_n;
+            way["name"="{name}"]["place"]({s},{w},{n},{e}) -> .city_w;
+            relation["name"="{name}"]["place"]({s},{w},{n},{e}) -> .city_r;
+        );
+        (
+        """
+        self._query_historic = """
+        node(around:{radius})["historic"] -> .historic_n;
+        way(around:{radius})["historic"] -> .historic_w;
+        relation(around:{radius})["historic"] -> .historic_r;
+        """
+        self._query_leisure = """
+        node(around:{radius})["leisure"] -> .leisure_n;
+        way(around:{radius})["leisure"] -> .leisure_w;
+        relation(around:{radius})["leisure"] -> .leisure_r;
+        """
+
+        self._query_tourism = """
+        node(around:{radius})["tourism"] -> .tourism_n;
+        way(around:{radius})["tourism"] -> .tourism_w;
+        relation(around:{radius})["tourism"] -> .tourism_r;
+        """
+        self._query_hotel = """
+        node(around:{radius})["building"="hotel"] -> .hotel_n;
+        way(around:{radius})["building"="hotel"] -> .hotel_w;
+        relation(around:{radius})["building"="hotel"] -> .hotel_r;
+        """
+
+        self._query_aerodrome = """
+        node(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_n;
+        way(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_w;
+        relation(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_r;
+        """
+        self._query_fin = """
         );
         out body;
         >;
         out skel qt;
-        """.format(name=name,
-                   n=lat_lng['lat'] + 1.0,
-                   s=lat_lng['lat'] - 1.0,
-                   w=lat_lng['lng'] - 1.0,
-                   e=lat_lng['lng'] + 1.0,
-                   radius=radius,
-                   radius_x_2=radius * 2)
+        """
+
+    def get_poi_cnt_for_city(self, name: str, lat_lng: {float, float}, radius: int):
+        query = (self._query_header
+                 + self._query_historic
+                 + self._query_leisure
+                 + self._query_tourism
+                 + self._query_hotel
+                 + self._query_aerodrome
+                 + self._query_fin).format(name=name,
+                                           n=lat_lng['lat'] + 1.0,
+                                           s=lat_lng['lat'] - 1.0,
+                                           w=lat_lng['lng'] - 1.0,
+                                           e=lat_lng['lng'] + 1.0,
+                                           radius=radius,
+                                           radius_x_2=radius * 2)
         while True:
             try:
                 result = self._api.query(query)
@@ -63,7 +87,7 @@ class OSM:
         return len(api_query_result.node_ids) + len(api_query_result.relation_ids) + len(api_query_result.way_ids)
 
     def _get_poi_cnt_for_city_low_memory(self, name: str, lat_lng: {float, float}, radius: int):
-        queries = OSM._build_low_memory_queries_poi_cnt_for_city(name, lat_lng, radius)
+        queries = self._build_low_memory_queries_poi_cnt_for_city(name, lat_lng, radius)
         cnt = 0
         for q in queries:
             while True:
@@ -81,54 +105,12 @@ class OSM:
                 break
         return cnt
 
-    @staticmethod
-    def _build_low_memory_queries_poi_cnt_for_city(name: str, lat_lng: {float, float}, radius: int):
-        header = """
-        [out:json][timeout:60];
-        (
-            node["name"="{name}"]["place"]({s},{w},{n},{e}) -> .city_n;
-            way["name"="{name}"]["place"]({s},{w},{n},{e}) -> .city_w;
-            relation["name"="{name}"]["place"]({s},{w},{n},{e}) -> .city_r;
-        );
-        (
-        """
-        historic = """
-        node(around:{radius})["historic"] -> .historic_n;
-        way(around:{radius})["historic"] -> .historic_w;
-        relation(around:{radius})["historic"] -> .historic_r;
-        """
-        leisure = """
-        node(around:{radius})["leisure"] -> .leisure_n;
-        way(around:{radius})["leisure"] -> .leisure_w;
-        relation(around:{radius})["leisure"] -> .leisure_r;
-        """
-        tourism = """
-        node(around:{radius})["tourism"] -> .tourism_n;
-        way(around:{radius})["tourism"] -> .tourism_w;
-        relation(around:{radius})["tourism"] -> .tourism_r;
-        """
-        hotel = """
-        node(around:{radius})["building"="hotel"] -> .hotel_n;
-        way(around:{radius})["building"="hotel"] -> .hotel_w;
-        relation(around:{radius})["building"="hotel"] -> .hotel_r;
-        """
-        aerodrome = """
-        node(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_n;
-        way(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_w;
-        relation(around:{radius_x_2})["aeroway"="aerodrome"] -> .aerodrome_r;
-        """
-        fin = """
-        );
-        out body;
-        >;
-        out skel qt;
-        """
-
-        queries = [header + historic + fin,
-                   header + leisure + fin,
-                   header + tourism + fin,
-                   header + hotel + fin,
-                   header + aerodrome + fin]
+    def _build_low_memory_queries_poi_cnt_for_city(self, name: str, lat_lng: {float, float}, radius: int):
+        queries = [self._query_header_vast + self._query_historic + self._query_fin,
+                   self._query_header_vast + self._query_leisure + self._query_fin,
+                   self._query_header_vast + self._query_tourism + self._query_fin,
+                   self._query_header_vast + self._query_hotel + self._query_fin,
+                   self._query_header_vast + self._query_aerodrome + self._query_fin]
         formatted_queries = []
         for q in queries:
             fq = q.format(name=name,
